681fe7d3749206baa6b3eca975743dd2c03bd869
David Whittington <djwhitt@gmail.com>
2021-05-23 16:31:48 -0500

    Add Babashka flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Babashka static binary package flake";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in rec {
+      packages.x86_64-linux.babashka-static-bin =
+        let
+          p = { stdenv, lib, fetchurl, ... }: pkgs.stdenv.mkDerivation rec {
+            name = "babashka-static-bin";
+            version = "0.4.3";
+
+            src = fetchurl {
+              url = "https://github.com/babashka/babashka/releases/download/v${version}/babashka-${version}-linux-amd64-static.tar.gz";
+              sha256 = "sha256-8POym1JtOY8nRBEfr7AdLoRkza2Dpnxe72+w9rCF448=";
+            };
+
+            #nativeBuildInputs = [
+            #  autoPatchelfHook
+            #  stdenv.cc.cc.lib
+            #  gmp
+            #  ncurses
+            #  openssl
+            #  zlib
+            #];
+
+            unpackPhase = ''
+              tar xvf $src
+            '';
+
+            installPhase = ''
+              mkdir $out
+              mv bb $out/
+            '';
+
+            meta = with lib; {
+              homepage = https://github.com/babashka/babashka;
+              description = "Native, fast starting Clojure interpreter for scripting";
+              platforms = platforms.linux;
+            };
+          };
+        in pkgs.callPackage p { };
+
+      defaultPackage.x86_64-linux = packages.x86_64-linux.babashka-static-bin;
+    };
+}
